a6a7f64558e701a0d32ad8db8ef633f81c38fc4a
lucamodica <luca.modica55@gmail.com>
2023-11-19 19:03:16 +0100

    feat(assignment2): code for comp ex2 done, have to be revised

--- assignment2/comp_ex2.m
+++ assignment2/comp_ex2.m
@@ -36,4 +36,41 @@ disp("value of ||Mv|| for view2: " + norm(cross(normPoints2, Xmodel) * sol2));
 % use the 2 solutions to compute P1 and P2
 P1 = reshape(sol1(1:12) ,[4 3])';
 P2 = reshape(sol2(1:12) ,[4 3])';
+% tranform the camera matrix to the original version
+P1orig = standardization_mat(pflat(x{1}))^(-1) * P1;
+P2orig = standardization_mat(pflat(x{2}))^(-1) * P2;
+% projection of the 3D model point into the respective cameras
+XModelNorm = [Xmodel; ones(1, size(Xmodel, 2))];
+xModelProjP1 = pflat(P1orig * XModelNorm);
+xModelProjP2 = pflat(P2orig * XModelNorm);
 
+
+% plot the projected point on the views
+figure;
+subplot(1, 2, 1);
+imagesc(im1);
+hold on;
+plot(xModelProjP1(1, :), xModelProjP1(2, :),'r*');
+subplot(1, 2, 2);
+imagesc(im2);
+hold on;
+plot(xModelProjP2(1, :), xModelProjP2(2, :),'r*');
+% the points appear to be close to each other in the image
+% coordinate origin
+
+
+% for each camera, plot the 3D model visualizing
+% camera center, principal axes and the 3D model points
+figure;
+subplot(1, 2, 1);
+plot_camera(P1orig, 2);
+hold on;
+plot3 ([Xmodel(1, startind ); Xmodel(1 , endind )], [Xmodel(2, startind ); Xmodel(2 , endind )], [ Xmodel(3, startind ); Xmodel(3 , endind )], 'b-' );
+subplot(1, 2, 2);
+plot_camera(P2orig, 2);
+hold on;
+plot3 ([Xmodel(1, startind ); Xmodel(1 , endind )], [Xmodel(2, startind ); Xmodel(2 , endind )], [ Xmodel(3, startind ); Xmodel(3 , endind )], 'r-' );
+
+% compute the instrinsic parameter of the 2 cameras
+rq1 = rq(P1orig);
+rq2 = rq(P2orig);
--- assignment2/normalize_points_for_DLT.m
+++ assignment2/normalize_points_for_DLT.m
@@ -1,15 +1,7 @@
 function [normalized_points] = normalize_points_for_DLT(points)
-    % pflat the points first
     ppoints = pflat(points);
 
-    % compute the necessary values for the
-    % normalization
-    meanX = mean(ppoints(1, :));
-    meanY = mean(ppoints(2, :));
-    stdX = std(ppoints(1, :));
-    stdY = std(ppoints(2, :));
+    N = standardization_mat(ppoints);
 
     % normalize points
-    normalized_points = ppoints;
-    normalized_points(1, :) = (ppoints(1, :) - meanX) / stdX;
-    normalized_points(2, :) = (ppoints(2, :) - meanY) / stdY;
+    normalized_points = N * ppoints;
